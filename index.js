faa7cd05ae03bdaf2dfb42bcb8239a8f2059e1dd
Abhi <abhigandhi918@gmail.com>
2026-07-29 22:55:18 +0530

    Add root route for health check

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get('/', (req, res) => {
+    res.send('Madadwala Backend is running!');
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
